fc4e5eda4cc7d91055ed240f967ae0f033fc71d9
seungjun-green <lsj3285007@gmail.com>
2022-11-09 08:19:49 +0900

    added raining animation

--- rain.js
+++ rain.js
@@ -1,26 +1,9 @@
-var makeItRain = function() {
-    //clear out everything
+const main = document.querySelector('main');
 
-    var increment = 0;
-    var drops = "";
-    var backDrops = "";
+let htmlString = "";
 
-    while (increment < 100) {
-        //couple random numbers to use for various randomizations
-        //random number between 98 and 1
-        var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
-        //random number between 5 and 2
-        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
-        //increment
-        increment += randoFiver;
-        //add in a new raindrop with various randomizations to certain CSS properties
-        drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-        backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-    }
-
-    $('.rain.front-row').append(drops);
-    $('.rain.back-row').append(backDrops);
+for (let i = 0; i < 10; i++) {
+    htmlString += `<div class="drop" style="margin-right:${i+10}px></div>`;
 }
 
-
-makeItRain();
+main.innerHTML = htmlString;
