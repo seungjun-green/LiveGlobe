0bc089f1c6409953173419352d3ee8b9dba9a296
seungjun-green <lsj3285007@gmail.com>
2022-11-12 15:31:26 +0900

    added search!

--- newScript.js
+++ newScript.js
@@ -1,16 +1,32 @@
+var map;
+var service;
+var infowindow;
+
 function initMap() {
+    var sydney = new google.maps.LatLng(-33.867, 151.195);
 
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 11,
-        center: { lat: 62.323907, lng: -150.109291 },
-        mapTypeId: "satellite",
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(
+        document.getElementById('map'), {center: sydney, zoom: 15});
+
+    var request = {
+        query: 'Museum of Contemporary Art Australia',
+        fields: ['name', 'geometry'],
+    };
+
+    var service = new google.maps.places.PlacesService(map);
+
+    service.findPlaceFromQuery(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
+            map.setCenter(results[0].geometry.location);
+        }
     });
-
-
-
-    updateWeather(map);
-
 }
+
 
 function updateWeather(map) {
     map.addListener("center_changed", () => {
--- rain.js
+++
@@ -1,9 +0,0 @@
-const main = document.querySelector('main');
-
-let htmlString = "";
-
-for (let i = 0; i < 10; i++) {
-    htmlString += `<div class="drop" style="margin-right:${i+10}px></div>`;
-}
-
-main.innerHTML = htmlString;
