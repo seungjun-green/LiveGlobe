569eada7b76de9a36dd596e79f29cc2232a2f29d
seungjun-green <lsj3285007@gmail.com>
2022-11-13 16:56:14 +0900

    ddd2

--- newScript.js
+++ newScript.js
@@ -239,11 +239,10 @@ function snowy() {
         let i = 0;
 
         while (i<amount) {
-            let drop = document.createElement('rainDrop');
+            let drop = document.createElement('snowDrop');
 
             let size = Math.random() * 5;
             let posX = Math.floor(Math.random() * window.innerWidth);
-            let delay = Math.random() * -20;
             let duration = Math.random() * 5;
 
             drop.style.width = 0.2 + size+'px';
